b485bcca93008830013d135b32300bd8f738de9d
Dhruba <ndhrubajyoti195@gmail.com>
2022-01-16 11:37:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/Textform';
+import ContactUs from './components/ContactUs';
+import News from './components/News';
+import React, { useState } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      {/* <Navbar/> */}
+      <Router>
+        <Navbar title="TEXTOPEDIA" aboutText="Contact Us" aboutText2="News" />
+
+        <div className="container my-3">
+          <Switch>
+            {/* /users --> Component 1
+        /users/home --> Component 2 */}
+            <Route exact path="/contactus">
+              <ContactUs />
+            </Route>
+
+            <Route exact path="/news">
+              <News />
+            </Route>
+
+            <Route exact path="/">
+              <TextForm heading="Enter the text to analyze below" />
+            </Route>
+
+          </Switch>
+        </div>
+        
+      </Router>
+    </>
+  );
+}
+
+export default App;
